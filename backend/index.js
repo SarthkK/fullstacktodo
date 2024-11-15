809588be8923895aef2cf16d4e25a8a311447ce1
Sarthk Kharwal <sarthkkharwal@gmail.com>
2024-11-15 12:19:18 +0530

    backend routes and types initialised

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post("/todo", (req, res) => {
-  if (createTodo.safeParse(req.body).success === true) {
-  } else {
+  if (!createTodo.safeParse(req.body).success) {
+    res.status(411).json({
+      msg: "you send the wrong inputs",
+    });
+    return;
   }
+  //   add to backend
 });
 app.get("/todo", (req, res) => {});
 app.put("/completed", (req, res) => {
-  if (updateTodo.safeParse(req.body).success === false) {
-  } else {
+  if (!updateTodo.safeParse(req.body).success) {
+    res.status(411).json({
+      msg: "wrong data inputs",
+    });
+    return;
   }
+  //   send to backend
 });
 
 app.listen(port, () => {
