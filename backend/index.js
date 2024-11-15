bea82ca4026884a3e126cd6016b297656ae11920
Sarthk Kharwal <sarthkkharwal@gmail.com>
2024-11-15 13:46:41 +0530

    backend completed

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,69 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { todo, todo } = require("./db");
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post("/todo", (req, res) => {
-  if (!createTodo.safeParse(req.body).success) {
+app.post("/todo", async (req, res) => {
+  const payload = req.body;
+  if (!createTodo.safeParse(payload).success) {
     res.status(411).json({
       msg: "you send the wrong inputs",
     });
     return;
   }
-  //   add to backend
+  try {
+    await todo.create({
+      title: payload.title,
+      description: payload.description,
+      completed: false,
+    });
+    res.json({
+      msg: "todo created",
+    });
+  } catch (error) {
+    res.json({
+      msg: "couldn't create todo",
+    });
+  }
 });
-app.get("/todo", (req, res) => {});
-app.put("/completed", (req, res) => {
-  if (!updateTodo.safeParse(req.body).success) {
+app.get("/todo", async (req, res) => {
+  try {
+    const todos = await todo.find({});
+    return res.status(200).json({ todos });
+  } catch (error) {
+    return res.json({
+      msg: "couldn't retrieve tasks",
+    });
+  }
+});
+app.put("/completed", async (req, res) => {
+  const payload = req.body;
+  if (!updateTodo.safeParse(payload).success) {
     res.status(411).json({
       msg: "wrong data inputs",
     });
     return;
   }
-  //   send to backend
+  try {
+    await todo.updateOne(
+      {
+        _id: payload.id,
+      },
+      { completed: true }
+    );
+
+    return res.status(200).json({
+      msg: "todo marked as completed",
+    });
+  } catch (error) {
+    return res.json({
+      msg: "error updating task",
+    });
+  }
 });
 
 app.listen(port, () => {
