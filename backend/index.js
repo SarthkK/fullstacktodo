cdedef679c52e1c99d1a9b81c64464e963b992e2
Sarthk Kharwal <sarthkkharwal@gmail.com>
2024-11-15 15:17:38 +0530

    initialized frontend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
-const { todo, todo } = require("./db");
+const { todo } = require("./db");
 const app = express();
 const port = 3000;
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { CreateTodo } from "./components/CreateTodo";
+import { Todos } from "./components/Todos";
+
+function App() {
+  return (
+    <>
+      <CreateTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
